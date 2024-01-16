af8d8928d929b929899e5f29911ae69afe968f2f
Phil Bates <philbates35@gmail.com>
2024-01-16 15:11:04 +0000

    JS: Install & configure typescript
    The tsconfig.json and vite-env.d.ts come from a fresh Vite project
    using the "vanilla-ts" template, while the tsconfig.node.json comes
    from the "react-ts" template. See:
    
    https://github.com/vitejs/vite/tree/3b7e0c3dd20d0d4f4c8d2d9aa93480724be8886d/packages/create-vite/template-vanilla-ts
    https://github.com/vitejs/vite/blob/3b7e0c3dd20d0d4f4c8d2d9aa93480724be8886d/packages/create-vite/template-react-ts

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -5,9 +5,8 @@
  */
 
 import axios from 'axios';
-window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
