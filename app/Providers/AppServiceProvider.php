2a77ebc9c765584852f7e6f3704165e93cc6d5db
Phil Bates <phil.bates@theshopworks.com>
2024-01-18 13:37:34 +0000

    Eloquent: Call Model::shouldBeStrict() when not in production
    See: https://laravel-news.com/shouldbestrict

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if ($this->isProduction()) {
+            Model::shouldBeStrict();
+        }
+    }
+
+    private function isProduction(): bool
+    {
+        $environment = $this->app->environment('production');
+
+        \assert(\is_bool($environment));
+
+        return $environment;
     }
 }
