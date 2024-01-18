51a8bbc7ec1e13f06f84c1e1d9076406f80ee2ea
Phil Bates <phil.bates@theshopworks.com>
2024-01-18 13:47:21 +0000

    Translation: Log a warning for any unknown lang keys
    Based on https://laravel.com/docs/10.x/localization#handling-missing-translation-strings.
    
    Use "resolving" callback instead of using the Log facade / the 'log'
    service because if we do that it will force the deferred
    LogServiceProvider to be registered on every request, essentially
    making it not deferred any more.
    
    Similar to how Model::shouldBeStrict() is only called when not
    in production, we only throw an exception when not in production.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Translation\Translator;
+use OutOfBoundsException;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,7 +17,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->resolving('translator', function (Translator $translator): void {
+            $translator->handleMissingKeysUsing(function (string $key): string {
+                $message = "Missing translation key [{$key}] detected.";
+
+                if (!$this->isProduction()) {
+                    throw new OutOfBoundsException($message);
+                }
+
+                Log::warning($message);
+
+                return $key;
+            });
+        });
     }
 
     /**
