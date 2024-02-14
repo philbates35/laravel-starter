f02b5b7a74c8d7d53602d8efbdd2cbea6b86828d
Phil Bates <philbates35@gmail.com>
2024-02-14 17:31:14 +0000

    Rector: Use new style of configuration

--- rector.php
+++ rector.php
@@ -5,13 +5,10 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\Php83\Rector\ClassConst\AddTypeToConstRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
-use Rector\Privatization\Rector\Class_\FinalizeClassesWithoutChildrenRector;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
 use RectorLaravel\Set\LaravelSetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__ . '/app',
         __DIR__ . '/bootstrap',
         __DIR__ . '/config',
@@ -19,29 +16,24 @@ return static function (RectorConfig $rectorConfig): void {
         __DIR__ . '/resources',
         __DIR__ . '/routes',
         __DIR__ . '/tests',
-    ]);
-
-    $rectorConfig->skip([
-        __DIR__ . '/bootstrap/cache',
-    ]);
-
+    ])
+    ->withRootFiles()
     // Larastan's bootstrap file doesn't run when Rector boots PHPStan, so we need to include it manually. See:
     //   * https://github.com/rectorphp/rector/issues/8006
     //   * https://github.com/larastan/larastan/issues/1664#issuecomment-1637152828
-    $rectorConfig->bootstrapFiles([__DIR__ . '/vendor/larastan/larastan/bootstrap.php']);
-    $rectorConfig->phpstanConfig(__DIR__ . '/phpstan.neon.dist');
-
-    $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_83,
-
-        SetList::CODE_QUALITY,
-        SetList::DEAD_CODE,
-        SetList::STRICT_BOOLEANS,
-        SetList::PRIVATIZATION,
-        SetList::TYPE_DECLARATION,
-        SetList::EARLY_RETURN,
-        SetList::INSTANCEOF,
-
+    ->withBootstrapFiles([__DIR__ . '/vendor/larastan/larastan/bootstrap.php'])
+    ->withPHPStanConfigs([__DIR__ . '/phpstan.neon.dist'])
+    ->withPhpSets()
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        privatization: true,
+        instanceOf: true,
+        earlyReturn: true,
+        strictBooleans: true,
+    )
+    ->withSets([
         LaravelSetList::LARAVEL_100,
         LaravelSetList::LARAVEL_CODE_QUALITY,
         LaravelSetList::LARAVEL_FACADE_ALIASES_TO_FULL_NAMES,
@@ -50,15 +42,11 @@ return static function (RectorConfig $rectorConfig): void {
         PHPUnitSetList::PHPUNIT_100,
         PHPUnitSetList::PHPUNIT_CODE_QUALITY,
         PHPUnitSetList::ANNOTATIONS_TO_ATTRIBUTES,
-    ]);
-
-    $rectorConfig->skip([
-        // Enforcing final on everything doesn't really offer much value, and stops up from
-        // mocking a concrete class, in the rare occasion we need to do so.
-        FinalizeClassesWithoutChildrenRector::class,
+    ])
+    ->withSkip([
+        __DIR__ . '/bootstrap/cache',
 
         // TODO Stop skipping this rule as soon as phpcs supports typed const
         //     See: https://github.com/PHPCSStandards/PHP_CodeSniffer/issues/106
         AddTypeToConstRector::class,
     ]);
-};
