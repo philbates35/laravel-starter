a81e365a8b1fca37c3bf81e891f6da535aa0d8b0
Phil Bates <philbates35@gmail.com>
2024-03-26 14:47:25 +0000

    Upgrade to Laravel 11
    Use the updated slim skeleton, as well as bumping all composer
    dependencies.

--- routes/console.php
+++ routes/console.php
@@ -6,17 +6,6 @@ use Illuminate\Console\Command;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-/*
-|--------------------------------------------------------------------------
-| Console Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of your Closure based console
-| commands. Each Closure is bound to a command instance allowing a
-| simple approach to interacting with each command's IO methods.
-|
-*/
-
 /**
  * See: https://github.com/larastan/larastan/issues/1110.
  *
@@ -24,4 +13,4 @@ use Illuminate\Support\Facades\Artisan;
  */
 Artisan::command('inspire', function (): void {
     $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+})->purpose('Display an inspiring quote')->hourly();
--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,4 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', static fn() => view('welcome'));
